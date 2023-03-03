69c4d0a60c10d614cfd191d734a5d3a2d7687759
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 17:34:17 +0300

    add lists unique

---
+++ src/routes/shared/lists/unique/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { unique } from "@sveu/shared/lists"
+
+	const list = [
+		{ id: 1, name: "John" },
+		{ id: 2, name: "Jane" },
+		{ id: 3, name: "Mila" },
+		{ id: 4, name: "Emma" },
+		{ id: 5, name: "Jack" },
+		{ id: 6, name: "Jack" },
+	]
+
+	const unique_names = unique(list, (item) => item.name)
+
+	console.log(unique_names)
+</script>
+
+<h1>Open the console</h1>
