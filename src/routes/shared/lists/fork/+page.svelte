bec5ab3e4940104f824aa3ca1d0af02f5d7ad934
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 17:15:48 +0300

    add lists fork

---
+++ src/routes/shared/lists/fork/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { fork } from "@sveu/shared/lists"
+
+	const actors = [
+		{ name: "Mila", age: 30 },
+		{ name: "John", age: 40 },
+		{ name: "Jane", age: 50 },
+		{ name: "Emma", age: 60 },
+	]
+
+	const [young, old] = fork(actors, (actor) => actor.age < 50) // [{name: "Mila", age: 30}, {name: "John", age: 40}] and [{name: "Jane", age: 50}, {name: "Emma", age: 60}]
+
+	console.log(young)
+
+	console.log(old)
+</script>
+
+<h1>open the console</h1>
