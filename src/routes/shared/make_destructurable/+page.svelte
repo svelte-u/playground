f905ea9d6bd9ac18662f6ab56c8099ec771c26e4
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 15:44:42 +0300

    add Make Destructurable

---
+++ src/routes/shared/make_destructurable/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { make_destructurable } from "@sveu/shared"
+
+	const foo = { name: "foo" }
+
+	const bar = 1024
+
+	const obj = make_destructurable({ foo, bar }, [foo, bar])
+
+	let { foo: foo1, bar: bar1 } = obj
+
+	let [foo2, bar2] = obj
+</script>
+
+<div class="text-center">
+	<h1>Object like</h1>
+	{foo1.name} is {bar1}
+	<hr />
+	<h1>Array like</h1>
+	{foo2.name} is {bar2}
+</div>
