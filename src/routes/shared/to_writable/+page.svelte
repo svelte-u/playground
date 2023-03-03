38ed71ad6626b4013d32e08845ce3a003eeb4d30
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 16:26:23 +0300

    add tryit

---
+++ src/routes/shared/to_writable/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { readable } from "svelte/store"
+
+	import { to_writable } from "@sveu/shared"
+
+	const num = readable(0)
+
+	const new_num = to_writable(num)
+
+	$new_num = 1
+</script>
+
+{$new_num}
---
+++ src/routes/shared/tryit/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { tryit } from "@sveu/shared"
+	let result: number | undefined, error: unknown
+
+	function add(a: number, b: number): number {
+		// uncomment the line blow
+		// throw Error("You can't add number here")
+		return a + b
+	}
+
+	const add_async = tryit(add)
+
+	async function run() {
+		const data = await add_async(1, 2)
+		result = data?.result ?? undefined
+		// @ts-ignore
+		error = data.error?.message
+	}
+
+	run()
+</script>
+
+{#if result}{result}{/if}
+{#if error}{error}{/if}
