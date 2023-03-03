60dda6de9f87901701079987a5400944463a6799
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 17:14:14 +0300

    add lists enhance

---
+++ src/routes/shared/lists/enhance/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { enhance } from "@sveu/shared/lists"
+
+	const arr = [1, 2, 3]
+
+	const list = enhance(arr)
+
+	// 		list.append(4)
+
+	// 		list.clear()
+
+	// 		list.append([1, 2, 3])
+
+	// 		console.log(list.count(1))
+
+	// 		const copy = list.copy()
+	// 		console.log($copy)
+
+	// 		console.log(list.index(2))
+
+	// 		list.insert(4, 1)
+
+	// 		list.remove(1)
+
+	// 		console.log(list.pop())
+
+	// 		list.sort({type: "number", reverse: true})
+</script>
+
+{$list}
