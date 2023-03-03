1d55f9e7cb104ef46d659f7d7ad31ad76ee7bc8c
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 16:55:42 +0300

    add lists cluster

---
+++ src/routes/shared/lists/cluster/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { cluster } from "@sveu/shared/lists"
+
+	let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+	const clusters = cluster(list, { size: 3 })
+
+	console.log(clusters)
+</script>
+
+<h1>open the console</h1>
