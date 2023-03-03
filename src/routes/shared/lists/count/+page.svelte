811be622e64b823ef8877b6dc62c741d9587c354
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 17:13:05 +0300

    add lists count

---
+++ src/routes/shared/lists/count/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { count } from "@sveu/shared/lists"
+
+	let list = [
+		{ id: 1, name: "John" },
+		{ id: 2, name: "Jane" },
+		{ id: 3, name: "John" },
+		{ id: 4, name: "John" },
+		{ id: 5, name: "Jane" },
+	]
+
+	const actors = ["Mila", "John", "Jane", "John", "John", "Mila", "Mila"]
+
+	const counts = count(list, (item) => item.name)
+
+	const counts2 = count(actors, (actor) => actor)
+
+	console.log(counts)
+
+	console.log(counts2)
+</script>
+
+<h1>open the console</h1>
