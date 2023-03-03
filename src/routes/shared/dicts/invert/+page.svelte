ac7a77c02989ca9e8e2b4109321b7d5091d78805
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 16:51:00 +0300

    add dicts invert

---
+++ src/routes/shared/dicts/invert/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { invert } from "@sveu/shared/dicts"
+
+	const obj = { Mila: "Kunis", Emma: "Watson", Scarlett: "Johansson" }
+
+	console.log(invert(obj))
+</script>
+
+<h1>open the console</h1>
