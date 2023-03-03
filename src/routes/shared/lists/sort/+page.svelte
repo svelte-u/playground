98457fe214e2b4d19de54bae304e5f0054706aa7
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 17:31:50 +0300

    add lists sort

---
+++ src/routes/shared/lists/sort/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { sort } from "@sveu/shared/lists"
+
+	const list = [
+		{ id: 1, name: "John" },
+		{ id: 2, name: "Jane" },
+		{ id: 3, name: "Mila" },
+		{ id: 4, name: "Emma" },
+		{ id: 5, name: "Jack" },
+	]
+
+	// change name with id
+	const sorted = sort(list, "name")
+
+	// change name with id
+	const sorted_desc = sort(list, "-name")
+
+	console.log(sorted)
+
+	console.log(sorted_desc)
+</script>
+
+<h1>Open the console</h1>
