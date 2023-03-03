4963da5a00250bb89feb1e9f16b42b60d0b2cd3d
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 16:34:32 +0300

    add curry chain

---
+++ src/routes/shared/curry/chain/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { chain } from "@sveu/shared/curry"
+
+	let result: any = ""
+
+	const chained = chain(
+		(a, b) => a + b,
+		(a) => a * 2
+	)
+</script>
+
+<h1>Result: {result}</h1>
+
+<button on:click="{() => (result = chained(7, 7))}">Sum</button>
