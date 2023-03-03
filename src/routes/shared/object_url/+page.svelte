fee4425f8f25d82509713288893a7db4b3bff9f1
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 15:50:22 +0300

    add Object Url

---
+++ src/routes/shared/object_url/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { object_url } from "@sveu/shared"
+
+	let file: File
+
+	const on_file_change = (e: any) => {
+		const target = e.target
+		const files = target.files
+		file = files && files.length > 0 ? files[0] : undefined
+	}
+
+	$: url = object_url(file)
+</script>
+
+<input type="file" on:change="{on_file_change}" />
+<br />
+
+{#if url}
+	<a href="{url}" target="_blank" rel="noreferrer">Open</a>
+{/if}
