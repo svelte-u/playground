902eb570f2b675339c1b1389ac0b5724897efe51
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 16:52:54 +0300

    add dicts picker

---
+++ src/routes/shared/dicts/picker/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { picker } from "@sveu/shared/dicts"
+
+	const obj = { Mila: "Kunis", Emma: "Watson", Scarlett: "Johansson" }
+
+	console.log(picker(obj, ["Mila", "Emma"]))
+</script>
+
+<h1>open the console</h1>
