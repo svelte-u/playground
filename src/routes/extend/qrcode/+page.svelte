ec20ed9a204ee9efd25c7c5ac7f22276eceda981
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 14:19:07 +0300

    add qrcode

---
+++ src/routes/extend/qrcode/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { qrcode } from "@sveu/extend/qrcode"
+
+	let value = "world"
+
+	$: ({ output, pending, error } = qrcode(value))
+</script>
+
+<div class="grid place-items-center">
+	<img src="{$output}" alt="qrcode" />
+	<br />
+	<p>Pending: {$pending}</p>
+	<p>Error: {$error}</p>
+	<br />
+	<input class="border border-4" bind:value="{value}" />
+</div>
