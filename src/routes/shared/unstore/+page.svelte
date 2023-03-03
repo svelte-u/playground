f2cacbf82c7929cd68157f079f9a2c13380bfb35
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 16:29:18 +0300

    unstore

---
+++ src/routes/shared/unstore/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { readable, writable } from "svelte/store"
+
+	import { unstore } from "@sveu/shared"
+
+	const num = readable(0)
+
+	const str = writable("Hello")
+
+	const list = [1, 2, "Hi"]
+</script>
+
+{unstore(num)}
+<br />
+{unstore(str)}
+<br />
+<br />
+{unstore(list)}
