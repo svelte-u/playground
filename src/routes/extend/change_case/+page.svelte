60dd91e22b8f0b531827e7c7ba590ca7f0ec29ae
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 11:03:34 +0300

    add Change Case

---
+++ src/routes/extend/change_case/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Default from "./eg1.svelte"
+	import WithOptions from "./eg2.svelte"
+</script>
+
+<h1 class="text-center">Change Case</h1>
+
+<Default />
+<!-- <WithOptions/> -->
---
+++ src/routes/extend/change_case/eg2.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { change_case } from "@sveu/extend/change_case"
+
+	const cases = [
+		"camelCase",
+		"capitalCase",
+		"constantCase",
+		"dotCase",
+		"headerCase",
+		"noCase",
+		"paramCase",
+		"pascalCase",
+		"pathCase",
+		"sentenceCase",
+		"snakeCase",
+	]
+
+	let value = "Svelte Action"
+	let _case: any = "snakeCase"
+</script>
+
+<h1 class="font-bold mt-5 text-lg text-center">
+	{change_case(value, _case, { delimiter: "-" })}
+</h1>
+<div class="space-x-3 mt-5">
+	{#each cases as c}
+		<button
+			on:click="{() => (_case = c)}"
+			class="rounded-xl bg-purple-600 text-white p-2">{c}</button>
+	{/each}
+</div>
