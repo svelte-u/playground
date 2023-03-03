b19c9873587df7f5ec33f88acbd90aa865e9d797
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 15:41:13 +0300

    add last_changed and len

---
+++ src/routes/shared/last_changed/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { last_changed, rtf } from "@sveu/shared"
+
+	const { value, timestamp } = last_changed(
+		"world",
+		+Date.now() - 1000 * 60 * 5
+	)
+
+	const time = rtf($timestamp)
+</script>
+
+<div class="text-center">
+	<input class="border border-4" bind:value="{$value}" />
+
+	<div>
+		Last changed: <strong>{$time}</strong> <span>({$timestamp})</span>
+	</div>
+</div>
---
+++ src/routes/shared/len/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { len } from "@sveu/shared"
+
+	const value = "Hello World"
+
+	const mapx = new Map()
+
+	const dict = {
+		a: 1,
+		b: 2,
+	}
+
+	const set = new Set([1, 2, 3])
+
+	const list = [1, 2, 3]
+
+	mapx.set("a", 1)
+
+	mapx.set("b", 2)
+</script>
+
+<h1>Length of "{value}" is {len(value)}</h1>
+
+<h1>Length of map is {len(mapx)}</h1>
+
+<h1>Length of dict is {len(dict)}</h1>
+
+<h1>Length of set is {len(set)}</h1>
+
+<h1>Length of list is {len(list)}</h1>
