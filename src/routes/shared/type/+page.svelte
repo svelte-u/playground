ea00a025c02d090eef73a1cd6b5940d5234ec0b6
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 16:27:57 +0300

    add type

---
+++ src/routes/shared/type/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { type } from "@sveu/shared"
+
+	const types = ["string", 1, 1.5, new Date(), new Set(), [], Symbol(), {}]
+</script>
+
+<br />
+{#each types as t}
+	<h1>{type(t)}</h1>
+	<hr class="border border-4" />
+{/each}
