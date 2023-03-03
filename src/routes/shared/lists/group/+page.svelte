7a440113ac782f2587c8701b6db98c005e238204
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 17:25:24 +0300

    add lists group

---
+++ src/routes/shared/lists/group/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { group } from "@sveu/shared/lists"
+
+	const actors = [
+		{ name: "Mila", age: 30 },
+		{ name: "John", age: 40 },
+		{ name: "Jane", age: 50 },
+		{ name: "Emma", age: 30 },
+	]
+
+	const grouped = group(actors, (actor) => actor.age)
+
+	console.log(grouped)
+</script>
