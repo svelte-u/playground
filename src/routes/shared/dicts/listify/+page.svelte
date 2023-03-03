b1daf1b1cb3954546c52dd91174e977cf0b2b26a
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 16:51:23 +0300

    add dicts listify

---
+++ src/routes/shared/dicts/listify/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { listify } from "@sveu/shared/dicts"
+
+	const obj = { Mila: "Kunis", Emma: "Watson", Scarlett: "Johansson" }
+
+	console.log(listify(obj, (key) => [key])) // keys only
+
+	console.log(listify(obj, (_, value) => [value])) // values only
+
+	console.log(listify(obj, (key, value) => [key, value])) // keys and values
+</script>
+
+<h1>Open console</h1>
