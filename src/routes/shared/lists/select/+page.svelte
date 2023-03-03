79dac28c0fc1621a151a12e90b058d3c536177c9
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 17:30:43 +0300

    add lists select

---
+++ src/routes/shared/lists/select/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { select } from "@sveu/shared/lists"
+
+	const list = [
+		{ id: 1, name: "John" },
+		{ id: 2, name: "Jane" },
+		{ id: 3, name: "Jack" },
+	]
+
+	const names = select(list, (item) => item.name)
+
+	const ids = select(
+		list,
+		(item) => item.name,
+		(item) => item.id > 1
+	)
+
+	console.log(names)
+
+	console.log(ids)
+</script>
+
+<h1>Open the console</h1>
