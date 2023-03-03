71de6ecc347efc2f9d65247ca8bf7e53288805d8
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 16:41:46 +0300

    add math random

---
+++ src/routes/shared/math/random/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { random } from "@sveu/shared/math"
+
+	let result: number = 0
+</script>
+
+<h1>{result}</h1>
+
+<button on:click="{() => (result = random(0, 100))}">Generate</button>
