2e3210f24127a2dc6fa489c3b84a79f7fa612c92
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 16:54:22 +0300

    add dicts pops

---
+++ src/routes/shared/dicts/pops/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { pops } from "@sveu/shared/dicts"
+
+	const obj = { Mila: "Kunis", Emma: "Watson", Scarlett: "Johansson" }
+
+	pops(obj, ["Mila", "Emma"])
+
+	console.log(obj)
+</script>
+
+<h1>open console</h1>
