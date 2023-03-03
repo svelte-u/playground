91e9f03c09224fc552ad68ef976337b4f9ee847f
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 16:13:43 +0300

    add template

---
+++ src/routes/shared/template/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { template } from "@sveu/shared"
+
+	const str = `Hi %name%,
+	Welcome to %platfrom%
+	`
+	const result = template(
+		str,
+		{ name: "Beatrice", platfrom: "svelte" },
+		/\%(.+?)\%/g
+	)
+</script>
+
+<h1 class="text-center">{result}!</h1>
