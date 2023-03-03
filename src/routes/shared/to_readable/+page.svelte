ae0841acebb1c20c31aae9761902af47330bca4c
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-03 16:22:25 +0300

    add to_readable

---
+++ src/routes/shared/to_readable/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { writable } from "svelte/store"
+
+	import { to_readable } from "@sveu/shared"
+
+	const num = writable(0)
+
+	const new_num = to_readable(num)
+	// uncomment the code below to see an error message
+	// $new_num = 1
+</script>
+
+{$new_num}
