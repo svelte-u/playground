23707e4645917f008b19c1301ea1a9b5d5a97a59
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-04 13:25:36 +0300

    add eye_dropper

---
+++ src/routes/browser/eye_dropper/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { eye_dropper } from "@sveu/browser"
+
+	const { supported, open, sRGBHex } = eye_dropper()
+</script>
+
+{#if $supported}
+	<div>
+		Supported: {$supported}
+	</div>
+
+	<div>
+		sRGBHex: <span style="color: {$sRGBHex?.replace(', 0)', ', 1)')}"
+			>{$sRGBHex}</span>
+	</div>
+
+	<button
+		disabled="{!$supported}"
+		on:click="{() => open()}"
+		class="rounded-md bg-sky-500 text-white p-3">
+		Open Eye Dropper
+	</button>
+{:else}
+	<span>Not Supported by Your Browser</span>
+{/if}
