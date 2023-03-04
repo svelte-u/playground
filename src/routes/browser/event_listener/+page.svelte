2449995f7c5b0d015abf5d36f12ddfb36f761760
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-04 13:16:37 +0300

    add event_listener

---
+++ src/routes/browser/event_listener/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	// import { event_listener } from "@sveu/browser"
+	import { on } from "@sveu/browser"
+	import { browser } from "@sveu/shared"
+
+	if (browser) {
+		const manual_cleanup = on(window, "keydown", (e: KeyboardEvent) => {
+			console.log(e.key)
+
+			if (e.key === "Escape") manual_cleanup()
+		})
+	}
+</script>
