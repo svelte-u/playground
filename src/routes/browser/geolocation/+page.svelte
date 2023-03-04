87dfe060f2d9212f52c1f46822ac1d3aba5a6e06
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-04 13:46:18 +0300

    add geolocation

---
+++ src/routes/browser/geolocation/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { geolocation } from "@sveu/browser"
+
+	const { coords, located_at, error, resume, pause, supported } =
+		geolocation()
+</script>
+
+<div class="grid place-items-center">
+	<h1>supported: {$supported}</h1>
+
+	<pre lang="json">{JSON.stringify(
+			{
+				coords: {
+					accuracy: $coords.accuracy,
+					latitude: $coords.latitude,
+					longitude: $coords.longitude,
+					altitude: $coords.altitude,
+					altitudeAccuracy: $coords.altitudeAccuracy,
+					heading: $coords.heading,
+					speed: $coords.speed,
+				},
+				located_at: $located_at,
+				error: $error ? $error.message : error,
+			},
+			null,
+			4
+		)}</pre>
+	<button class="rounded-md bg-red-600 text-white p-3" on:click="{pause}">
+		Pause watch
+	</button>
+	<br />
+	<button class="rounded-md bg-green-600 text-white p-3" on:click="{resume}">
+		Start watch
+	</button>
+</div>
