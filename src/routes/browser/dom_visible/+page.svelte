3cddb95d1a132f03dad1d9cd39f39a0a95309139
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-04 13:08:24 +0300

    add DOM Visible

---
+++ src/routes/browser/dom_visible/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { dom_visible } from "@sveu/browser"
+
+	import { timeoutfn } from "@sveu/shared"
+
+	const visible = dom_visible()
+
+	const startMessage = "💡 Minimize the page or switch tab then return"
+
+	let message = startMessage
+
+	const timeout = timeoutfn(() => {
+		message = startMessage
+	}, 1)
+
+	$: if ($visible === "visible") {
+		message = "🎉 Welcome back!"
+		timeout.start()
+	}
+</script>
+
+<h1 class="text-center">{message}</h1>
