e4bbd63d29a914efbbaa238d58291ad636a63d33
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-04 14:00:38 +0300

    add image

---
+++ src/routes/browser/image/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { image } from "@sveu/browser"
+	import { browser } from "@sveu/shared"
+
+	const src =
+		"https://i.ibb.co/JjL8tSt/final-fantasy-xiii-2-lightning-2560x1440.jpg"
+	const { loading } = image({
+		src,
+	})
+</script>
+
+<div class="grid place-items-center">
+	{#if !$loading}
+		<img
+			src="{src}"
+			alt="FF XIII-2 Lightning"
+			loading="lazy"
+			class="rounded-2xl h-66.25 w-90" />
+	{:else if !browser}
+		<!-- fallback if js is disabled -->
+		<img
+			src="{src}"
+			alt="FF XIII-2 Lightning"
+			loading="lazy"
+			class="rounded-2xl h-66.25 w-90" />
+	{:else}
+		<div style="background-color: #f1c91a" class="rounded-2xl h-66.25 w-90">
+		</div>
+	{/if}
+</div>
