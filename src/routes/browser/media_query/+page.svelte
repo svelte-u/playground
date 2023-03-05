a37ba4bb8efd21298aa01b8e0da8c922a47415a7
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-05 09:47:24 +0300

    add Media Query

---
+++ src/routes/browser/media_query/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { media_query } from "@sveu/browser"
+
+	const dark = media_query("(prefers-color-scheme: dark)")
+
+	const reduced = media_query("(prefers-reduced-motion: reduce)")
+
+	const more = media_query("(prefers-contrast: more)")
+
+	const less = media_query("(prefers-contrast: less)")
+
+	const custom = media_query("(prefers-contrast: custom)")
+
+	let contrast = "no-preference"
+
+	$: if ($more) contrast = "more"
+
+	$: if ($less) contrast = "less"
+
+	$: if ($custom) contrast = "custom"
+
+	$: if (!$more && !$less && !$custom) contrast = "no-preference"
+</script>
+
+<div class="text-center">
+	<h1>Prefers Dark: {$dark}</h1>
+
+	<h1>Prefers Reduce Motion: {$reduced}</h1>
+
+	<h1>Prefers Contrast: {contrast}</h1>
+</div>
