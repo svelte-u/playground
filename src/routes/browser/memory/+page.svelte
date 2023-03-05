609cb77fec1fc4ac57498abbf835b823d39cdcbe
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-05 09:49:06 +0300

    add memory

---
+++ src/routes/browser/memory/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { memory } from "@sveu/browser"
+
+	const { supported, result } = memory()
+</script>
+
+<div class="text-center">
+	<h1>Supported: {$supported}</h1>
+
+	<h2>Js Heap Size Limit: {$result?.jsHeapSizeLimit}</h2>
+
+	<h2>Total Js Heap: {$result?.totalJSHeapSize}</h2>
+
+	<h2>Used Js Heap: {$result?.usedJSHeapSize}</h2>
+</div>
