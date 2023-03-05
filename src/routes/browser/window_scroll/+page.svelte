a5a668e9fc29674fb95c180a96c5a9ed7644951b
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-05 12:05:32 +0300

    add window_scroll

---
+++ src/routes/browser/window_scroll/+page.svelte
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+	import { window_scroll } from "@sveu/browser"
+
+	const { x, y } = window_scroll()
+</script>
+
+<div class="flex flex-col h-screen w-screen items-center justify-center">
+	x: {$x}<br />
+	y: {$y}
+</div>
