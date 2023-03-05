4b51cf6c6a4866a9fa946e1e3dddf800ee0d8d66
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-05 09:51:46 +0300

    add mouse

---
+++ src/routes/browser/mouse/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { mouse } from "@sveu/browser"
+
+	const { x, y, source_type } = mouse()
+</script>
+
+<div class="text-center">
+	<h1>Mouse X: {$x}</h1>
+
+	<h2>Mouse Y: {$y}</h2>
+
+	<h2>Source Type: {$source_type}</h2>
+</div>
