e3ffdff0f9a66107a4b12e8f2a1a38dd2f7c929c
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-05 11:59:35 +0300

    add window_focus

---
+++ src/routes/browser/window_focus/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { window_focus } from "@sveu/browser"
+
+	const focused = window_focus()
+
+	const start_message =
+		"💡 Click somewhere outside of the document to unfocus."
+
+	let message = start_message
+
+	$: if (!$focused) {
+		message = "ℹ Tab is unfocused"
+	} else {
+		message = start_message
+	}
+</script>
+
+<h1 class="text-center">{message}</h1>
