2b8f2386197e7724bfc8166a5e82c6ce009dfc86
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-05 11:47:38 +0300

    add useragent

---
+++ src/routes/browser/useragent/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { useragent } from "@sveu/browser"
+
+	const {
+		supported,
+		mobile,
+		model,
+		platform,
+		platform_version,
+		bitness,
+		arch,
+		brands,
+	} = useragent()
+</script>
+
+<div class="text-center">
+	<h1>Supported: <small>{$supported}</small></h1>
+
+	<h1>Is mobile: <small>{$mobile}</small></h1>
+
+	<h1>Architecture: <small>{$arch}</small></h1>
+
+	<h1>Model: <small>{$model}</small></h1>
+
+	<h1>Platform: <small>{$platform}</small></h1>
+
+	<h1>Platform version: <small>{$platform_version}</small></h1>
+
+	<h1>Bitness: <small>{$bitness}</small></h1>
+
+	<h1>Brands: <small>{JSON.stringify($brands)}</small></h1>
+</div>
