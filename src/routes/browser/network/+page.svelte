7c0b3fb68d40c56c801c37d70bb758de5bf92e7d
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-05 11:05:11 +0300

    add network

---
+++ src/routes/browser/network/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { network } from "@sveu/browser"
+	import { strftime } from "@sveu/shared"
+
+	const {
+		online,
+		offline_at,
+		online_at,
+		downlink,
+		downlink_max,
+		effective_type,
+		rtt,
+		save_data,
+		type,
+	} = network()
+</script>
+
+<h4>online: {$online}</h4>
+<h4>online_at: {$online_at ? strftime($online_at, "%c") : ""}</h4>
+<h4>offline_at: {$offline_at ? strftime($offline_at, "%c") : ""}</h4>
+<h4>downlink: {$downlink}</h4>
+<h4>downlink max: {$downlink_max}</h4>
+<h4>effective_type: {$effective_type}</h4>
+<h4>rtt: {$rtt}</h4>
+<h4>save_data: {$save_data}</h4>
+<h4>type: {$type}</h4>
