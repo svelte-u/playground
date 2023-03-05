f0ef6e516138f0b07c48969c2d61a24a1c479aff
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-05 11:45:50 +0300

    add support

---
+++ src/routes/browser/support/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { support } from "@sveu/browser"
+
+	const supported = support("clipboard")
+
+	// 	const supported = support("memory", "performance")
+</script>
+
+<h1 class="text-center">Supported: {$supported}</h1>
