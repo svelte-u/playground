ad72801698380a2d66836c3e1f99b7939c5b8d45
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-06 13:08:38 +0300

    add

---
+++ src/routes/shared/curry/partial/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { partial } from "@sveu/shared/curry"
+
+	const add = (a: number, b: number, c: number) => a + b + c
+
+	const add_one = partial(add, 1)
+
+	const add_two = partial(add_one, 2)
+
+	console.log(add_two(3))
+</script>
+
+<h1 class="text-center">Open the console</h1>
