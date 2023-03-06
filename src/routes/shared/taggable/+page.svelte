03a7b4b94c46a702d0544d27e6fba7bc73dd7338
Mohamed-Kaizen <m.n.kaizen@gmail.com>
2023-03-06 13:14:08 +0300

    add taggable

---
+++ src/routes/shared/taggable/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { taggable } from "@sveu/shared"
+
+	const { toggled, toggle } = taggable()
+</script>
+
+<div class="flex flex-col space-y-3 mx-20 mt-20 text-center">
+	<h1>{$toggled ? "On" : "Off"}</h1>
+
+	<button
+		on:click="{() => toggle()}"
+		class="rounded-xl bg-purple-600 text-white p-3">Toggle</button>
+</div>
